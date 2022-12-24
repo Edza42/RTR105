2bb12fcbb89742831dae54cfda8c926e1d9700aa
Edza42 <eduards.zeile@inbox.lv>
2022-12-24 14:15:08 +0200

    Class

--- while_factorial.c
+++ while_factorial.c
@@ -1,1 +1,9 @@
 #include<stdio.h>
+
+int main(){
+  long int c,i,l,a,b;
+
+  printf("Lūdzu, ievadiet decimālu ciparu:");
+  scanf("%ld",&a);
+  printf("Lūdzu, izvēlieties vienu datu tipu (char-c; int-i; long long-l)\n:");
+  scanf("%ld",&b);
