3c342e20ec65b9d40f2ceb20f3e6d2139acf7a95
Edza42 <eduards.zeile@inbox.lv>
2022-12-26 15:18:50 +0200

    Class

--- for_factoial.c
+++ for_factoial.c
@@ -1,1 +1,25 @@
 #include<stdio.h>
+
+int main(){
+  long int a,b,c,i,l;
+  printf("Ievadiet vienu decimālu skaitli:");
+  scanf("%ld",&a);
+  printf("Izvēlieties datu tipu (char-c; int-i; long long-l)\n:");
+  scanf("%ld",&b);
+
+  int x,y;
+  y=1;
+  if(b=c, a>=1, a<=15){
+    printf("Nav iespējams aprēķināt faktoriāļa vērtību\n");
+  }
+  if(b=i, a>=1, a<=15){
+    printf("Nav iespējams aprēķināt faktoriāļa vērtību\n");
+  }
+  if(b-l, a>=1, a<=15){
+    for(x=1; x<=a; x++){
+       y=y*x;
+    }
+   printf("Faktoriāļa vērtība no %ld: %d",a,y);
+  }
+return 0;
+}
--- while_factorial.c
+++ while_factorial.c
@@ -7,3 +7,22 @@ int main(){
   scanf("%ld",&a);
   printf("Lūdzu, izvēlieties vienu datu tipu (char-c; int-i; long long-l)\n:");
   scanf("%ld",&b);
+
+int x,y;
+  x=y=1;
+if(b=c, a>=1, a<=15){
+  printf("Nav iespējams aprēķināt faktoriāla vērtību\n");
+  }
+if(b=i, a>=1, a<=15){
+  printf("Nav iespējams aprēķināt faktoriāļa vērtību\n");
+  }
+if(b=l, a>=1, a<=15){
+  while(x<=a){
+    y*=x;
+    x++;
+    }
+  printf("Faktoriāla vērtība no %ld: %d",a,y);
+}
+
+return 0;
+}
